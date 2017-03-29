7d335dd5a693202652da6168889c654aa522c77f
daxiahy <daxiahy@163.com>
2017-03-29 17:08:23 +0800

    Update html_parser.py
    词条将/view访问变为/item

--- html_parser.py
+++ html_parser.py
@@ -24,11 +24,11 @@ class HtmlParser(object):
     def _get_new_urls(self, page_url, soup):
         new_urls = set()
         #获取是词条的url链接
-        links = soup.find_all('a', href=re.compile(r"/view/\d+\.htm"))
+        links = soup.find_all('a', href=re.compile(r"/item/.+?"))
         for link in links:
             new_url = link['href']
             #拼接url
-            new_full_url = urlparse.urljoin(page_url, new_url)
+            new_full_url = urlparse.urljoin('http://baike.baidu.com', new_url)
             new_urls.add(new_full_url)
         #返回出有效urls
         return new_urls
